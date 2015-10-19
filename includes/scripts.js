4480776c97cbc06b48a5e3f95913ad6dcea4f04c
elfirl <mike.bilter@gmail.com>
2015-10-18 22:15:14 -0400

    Part 2 Completed
    Got the name working to print in the DOM. Struggled quite a bit on this
    part.

--- includes/scripts.js
+++ includes/scripts.js
@@ -1,6 +1,6 @@
 function whatNumber() {
 	var userNumber = parseInt(document.getElementById("numberInput").value);
-	
+
 	if(userNumber < 10) {
 		alert("Your number is less than 10!")
 	} else if(userNumber > 20) {
@@ -11,5 +11,11 @@ function whatNumber() {
 	else {
 		alert("You didn't pick a number. Please pick a number between 1 and 20.");
 	}
+}
+
+
+function whatName() {
+	var userName = document.getElementById("nameInput").value;
+	document.getElementById("nameOutput").innerHTML ="<p>" + "Is your name really " + userName + "?" + "</p>";
 }
 
