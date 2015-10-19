7c3444ca1cc6a9f9ddb122579ad8713f3980ce4c
elfirl <mike.bilter@gmail.com>
2015-10-18 21:38:20 -0400

    Part One Working
    Finally figured out how to get the function for the first part of the
    assignment working 100%.

--- includes/scripts.js
+++ includes/scripts.js
@@ -1,7 +1,15 @@
-function whatNumber () {
-	var userNumber = document.getElementById("numberInput");
-	if (userNumber < 10 )
-		alert ("Your number is less than 10!")
-	else
-		alert("You picked " + userNumber + " which is more than 10!")
+function whatNumber() {
+	var userNumber = parseInt(document.getElementById("numberInput").value);
+	
+	if(userNumber < 10) {
+		alert("Your number is less than 10!")
+	} else if(userNumber > 20) {
+		alert("Your number is larger than 20, please pick a number between 1 and 20.");
+	} else if(userNumber > 10 && userNumber < 20) {
+		alert("You picked " + userNumber + " which is more than 10!");
+	}
+	else {
+		alert("You didn't pick a number. Please pick a number between 1 and 20.");
+	}
 }
+
